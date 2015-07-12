20fab8872612a660590f14fe9f6eb665b19c4f9d
Casey <caseycodes@gmail.com>
2015-07-12 13:44:23 -0700

    redo sqldump

--- query.php
+++ query.php
@@ -2,16 +2,6 @@
 
 $query = $_GET["searchbox"];
 
-$severname = "localhost";
-$username = "foodie";
-$password = "secret_pass";
-$dbname = "food";
-
-// make connection
-$conn = mysql_connect($servername, $username, $password);
-@mysql_select_db($dbname);
-
-
 $sql = "SELECT * FROM food WHERE name LIKE '%$query%'";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_array($result)){
