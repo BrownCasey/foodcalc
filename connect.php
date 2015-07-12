ffaeb712071c6f169b04a738a7627830180da507
Casey <caseycodes@gmail.com>
2015-07-12 17:41:30 -0400

    fix boolean error

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 // connect.php to foodcalc database
 require_once("/var/www/food_constants.php");
 
-if(($connection = @mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD)) === $false)
+if(($connection = @mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD)) === false)
     print "could not connect to the server";
     
 if(@mysql_select_db(DB_NAME, $connection) === false)
